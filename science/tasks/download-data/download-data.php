d46ea03094c7b5004f74bdf82dc245983dbecf2f
starstryder <starstryder@gmail.com>
2019-08-05 17:44:43 -0500

    Begin Data Download forms - adding rest of initial form

--- science/tasks/download-data/download-data.php
+++ science/tasks/download-data/download-data.php
@@ -9,24 +9,34 @@
 
 <form id="DataFormat" action="<?php echo $BASE_URL;?>/science/download-data/download-data.php">
 
+    <h3>Select Data Download Options</h3>
+
     <!-- Select Project -->
+    <p><strong>Project:</strong><br/>
     <?php
-    $query = "SELECT name, id FROM applications WHERE active = 1";
+    $query = "SELECT title, id FROM applications WHERE active = 1";
     $results = $db->runQuery($query);
 
     if ($results === FALSE ) echo "No applications found";
     else {
         foreach ($results as $result) {
-            echo $result['title'] . "<br/>";
+            ?>
+            <select name="app_id">
+                <option value="<?php echo $result['id'];?>"><?php echo $result['title'];?></option>
+            </select></p>
+            <?php
         }
     }
     ?>
 
 
     <!-- Individual / Combined -->
+    <p><strong>Kind of Marks</strong><br/>
+    <input type="radio" name="combined" value="TRUE" checked>   Combined Marks &nbsp; &nbsp;
+    <input type="radio" name="combined" value="FALSE">          Individual Marks
+    </p>
 
-
-
+    <input type="submit" value="go">
 
 
 
