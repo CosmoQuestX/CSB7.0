05a11968347d9fc0291310f5113f3e63c42d18bc
starstryder <starstryder@gmail.com>
2019-08-05 17:09:38 -0500

    Begin Data Download forms

---
+++ science/tasks/download-data/download-data.php
@@ -0,0 +1,34 @@
+<?php
+// Start with the form
+
+// Lot for $_POST variables and output file if variables exist
+
+
+?>
+
+
+<form id="DataFormat" action="<?php echo $BASE_URL;?>/science/download-data/download-data.php">
+    <!-- Select Project -->
+
+    ?>
+
+    <?php
+    $query = "SELECT name, id FROM applications WHERE active = 1";
+    $results = $db->runQuery($query);
+
+    foreach($results as $result) {
+        echo $result['title'] . "<br/>";
+    }
+
+    ?>
+
+
+    <!-- Individual / Combined -->
+
+
+
+
+
+
+
+</form>
