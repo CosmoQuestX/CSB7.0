34554414924328b393adbdaad931c93be23f8793
Pamela Gay <plg@psi.edu>
2019-08-05 02:47:03 +0000

    adding image details

--- science/tasks/get-image-details/get-image-details.php
+++ science/tasks/get-image-details/get-image-details.php
@@ -15,17 +15,53 @@
     if (isset($_GET['image_set_name'])) {
         $name = $_GET['image_set_name'];
 
-        ?>
-        <div style="margin-top:30px">
+?>
+	<div style="float:right; width:48%;">
+	<img src="https://s3.amazonaws.com/cosmoquest/data/mappers/osiris/ImageDelivery_20190520/RAWIMAGES/<?php echo $name;?>" style="width:100%"> 
+	</div>
+        <div style="margin-top:30px; width: 50%;">
         <?php
         echo "<p> The following details are found for <strong>".$_GET['image_set_name']."</strong></p>";
 
         // Get image set id
-        $query  = "SELECT * FROM image_sets WHERE id=4209";
+        $query  = "SELECT * FROM image_sets WHERE name like '".$_GET['image_set_name']."'";
 
         $resultset = $db->runQuery($query);
-        $row = mysqli_fetch_assoc($result);
-        echo "Image id: ", $row['id'];
+
+	$image_set_id = $resultset[0]['id'];
+	
+        echo "<p><strong>Image id:</strong> ", $image_set_id."<br/>";
+
+
+	// Get images in that set
+	echo "<p><strong>Including sub-images:</strong> ";
+	$query = "SELECT * FROM images WHERE image_set_id = $image_set_id";
+        $resultset = $db->runQuery($query);
+	foreach($resultset as $result) {
+		echo $result['id']." ";
+		$images[] = $result['id'];
+	}
+	echo "</p>";
+
+
+	// Get username and email of people who mapped this image
+	echo "<p><strong>This master image was marked by: </strong></p>"; 
+	$query = "SELECT distinct(users.name), users.email 
+		  FROM users, image_users
+		  WHERE (image_users.image_id = ";
+	foreach($images as $image) {
+		$query .= $image. " OR image_users.image_id =";
+	}
+	$query = substr($query, 0, -25);
+
+	$query .= ") AND image_users.user_id = users.id ORDER BY users.name";
+	$resultset = $db->runQuery($query);
+	echo "<table style='color: black; font-size: 0.9em; width:100%;'>";
+	foreach($resultset as $result) {
+		echo "<tr><td>".$result['name']."</td><td>".$result['email']."</td></tr>";
+	}
+	echo "</table>";
+
         ?>
         </div>
         <?php
