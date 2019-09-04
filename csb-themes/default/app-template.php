526895933570d1a3cd207402d2f9fd068e7edfc4
dpi209 <dpi209@gmx.de>
2019-09-04 20:58:53 +0200

    FIX: Made the HTML comments actually closed

--- csb-themes/default/app-template.php
+++ csb-themes/default/app-template.php
@@ -18,7 +18,7 @@ $lang = json_decode($lang, true);
 <div id="main">
     <div class="container">
 
-        <!-- Left block -----------------------------------------------------------------!>
+        <!-- Left block ----------------------------------------------------------------->
         <div id="app-left" class="left">
             <?php $txt = $lang['app_page']['text-boxes']['app-left']; ?>
             <h2><?php echo $txt['title']; ?></h2>
@@ -55,7 +55,7 @@ $lang = json_decode($lang, true);
             </div>
         </div>
 
-        <!-- main block -----------------------------------------------------------------!>
+        <!-- main block ----------------------------------------------------------------->
         <div id="app-main" class="left">
 
             <div id="app-button-container" class="left">
@@ -76,7 +76,7 @@ $lang = json_decode($lang, true);
             <div class="clear"></div>
         </div>
 
-        <!-- Right block ----------------------------------------------------------------!>
+        <!-- Right block ---------------------------------------------------------------->
         <?php $txt = $lang['app_page']['text-boxes']['app-right']; ?>
         <div id="app-right" class="right">
         <h1><?php echo $txt['title'];?></h1>
