368a8df1e3cefa3da7fc3273b558cdffb22f93c6
dpi209 <dpi209@gmx.de>
2019-09-06 22:33:41 +0200

    Add input checking and basic sanitization

--- extras/tasks/name_features/name_features.php
+++ extras/tasks/name_features/name_features.php
@@ -33,7 +33,7 @@ if(isset($_GET['markX'])){
     echo "</table><style>#submission-data td,th {color:black; border: 1px black solid;}</style>";
 }
 
-if (!isset($_GET['image_set_name'])) {
+if (!isset($_GET['image_set_name']) || $_GET['image_set_name'] == "") {
     ?>
     <form id="GetImageToName" action="<?php echo $BASE_URL; ?>/extras/">
         <input type="hidden" name="task" value="name_features">
@@ -45,8 +45,8 @@ if (!isset($_GET['image_set_name'])) {
 
 
 // Check if an image was already submitted
-if (isset($_GET['image_set_name'])) {
-    $name = $_GET['image_set_name'];
+if (isset($_GET['image_set_name']) && $_GET['image_set_name'] !== "") {
+    $name = basename(preg_replace("/[.,;\\(]/", "", filter_input(INPUT_GET,'image_set_name',FILTER_SANITIZE_FULL_SPECIAL_CHARS,0)));
 
     ?>
     <div style="float:right; width:450px;">
