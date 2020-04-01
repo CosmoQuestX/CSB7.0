e83adb3fcde67adef1fafa68115b92e89722b881
dpi209 <dpi209@gmx.de>
2020-04-01 23:59:33 +0200

    Doing the fail whale
    Added a page that loads an error image depending on the error code it
    gets called with. Added an image for 403 and 404.

---
+++ csb/error/error.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Error file
+ */
+
+// We need our basic configuration first.
+require_once '../csb-loader.php';
+require_once($DB_class);
+require_once($BASE_DIR . "csb-accounts/auth.php");
+
+// Also, we should try to get the referring URL if possible, I guess?
+if(isset($_SESSION['referringURL'])) {
+    $referrer=filter_var($_SESSION['refererringURL'],FILTER_SANITIZE_URL,0);
+}
+
+// Check if the person is logged in
+$db = new DB($db_servername, $db_username, $db_password, $db_name);
+
+global $user;
+$user = isLoggedIn($db);
+
+// Now let's see which error we have been called with
+$error=filter_input(INPUT_GET, 'error', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+// Load the view
+global $page_title, $header_title, $SITE_TITLE;
+
+require_once($BASE_DIR . "/csb-content/template_functions.php");
+
+loadHeader($page_title);
+
+?>
+<div id='container-md'>
+	<div class='row'>
+        <div class="center">
+        	<div id='app-error' class="col-md">
+        		<img src='<?php echo $THEME_URL . "images/" . $error; ?>.png' alt='Error image for error <?php echo $error?>.'>
+        		<div id='app-error-btn' class="center">
+        		<?php if(isset($referrer)) {
+        		?>
+        		<a id="alert-button-back" class="btn btn-primary" href="<?php echo $referrer; ?>">Go back</a>
+        		<?php 
+        		}
+        		?>
+        		<a id="alert-button-home" class="btn btn-primary" href="<?php echo $BASE_URL; ?>">Go to the home page</a>
+        		</div>
+        	</div>
+        </div>
+	</div>
+</div>
+
+<?php 
+loadFooter();
+// Only one header please
+
+?>
