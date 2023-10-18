57d4ca3c6e0108d621581c624855e79d4f391482
Pamela Gay <pamela@starstryder.com>
2023-10-18 14:39:47 -0500

    - F getting a test test testing Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- tests/BuildTest.php
+++ tests/BuildTest.php
@@ -3,15 +3,16 @@ declare(strict_types=1);
 
 namespace CosmoQuestX\Tests;
 
-use CosmoQuestX\Tests\email;
+use CosmoQuestX\Applesauce;
 use PHPUnit\Framework\TestCase;
+
 class BuildTest extends TestCase
 {
     /** @test */
     public function testingTest()
     {
-
-        $this->assertEquals(3, 3);
+        $applesauce = new Applesauce();
+        $this->assertEquals(3, $applesauce->TheWordThree());
     }
 
 
