4461ec9e11f74907b1a6039c627cb5a43971ba54
Pamela Gay <pamela@starstryder.com>
2023-11-01 13:42:22 -0500

    . r moved namespace
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>

--- src/Applesauce.php
+++ src/Applesauce.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace unnamed;
+namespace CosmoQuestX;
 class Applesauce
 {
     public function TheWordThree(): int
--- tests/BuildTest.php
+++ tests/BuildTest.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace CosmoQuestX\Tests;
 
-use unnamed\Applesauce;
+use CosmoQuestX\Applesauce;
 use PHPUnit\Framework\TestCase;
 
 class BuildTest extends TestCase
