584131e211eae82e2ebf23400d550063cdd606bd
TheRealFakeAdmin <therealfakedev@gmail.com>
2024-01-03 17:36:22 -0500

    . d fixed grammar

--- csb/csb-accounts/register.php
+++ csb/csb-accounts/register.php
@@ -54,11 +54,11 @@ $notes = "Put Instructions Here";
 $notes = "
     <h5>How we use your information</h5>
     <p>
-    This is a citizen science site. Your contributes through our app will be
+    This is a citizen science site. Your contributions through our app will be
     used to produce new science results or to accomplish tasks with spacecraft.
     </p>
     <ul>
-    <li><span class='emp'>Your username</span> will be used to give you credit for your work. 
+    <li><span class='emp'>Your username</span> will be used to give you credit for your work.
     It will be visible in galleries and on lists of achievements.</li>
     <li><span class='emp'>Your email</span> will never be shared! We will contact
     you about how your work is used, and we will send you periodic newsletters
