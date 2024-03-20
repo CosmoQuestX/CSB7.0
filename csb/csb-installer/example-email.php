477c1450dd755454def9478f6bd01e5503135fa4
TheRealFakeAdmin <therealfakedev@gmail.com>
2024-03-20 19:12:20 -0400

    Revert "@ B Nuking the example email"
    This reverts commit 2a0ed32c93a65bacd6f7a0560bb425f8d7df6f8a.

--- csb/csb-installer/example-email.php
+++ csb/csb-installer/example-email.php
@@ -11,35 +11,38 @@ $mail = new PHPMailer(true);
 if (isset($_POST) && !empty($_POST)) {
     try {
         //Server settings
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp-relay.gmail.com';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'cosmoquestx@psi.edu';                     //SMTP username
-        $mail->Password   = 'bawkhrgqkcjkhsii';                               //SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
+//    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                          //Enable verbose debug output - Echos to output & causes SyntaxError, only use if viewing network log
+        $mail->isSMTP();                                                //Send using SMTP
+        $mail->Host = $_POST['email_host'];                                   //Set the SMTP server to send through
+        $mail->SMTPAuth = true;                                         //Enable SMTP authentication
+        $mail->Username = $_POST['email_username'];                           //SMTP username
+        $mail->Password = $_POST['email_password'];                           //SMTP password
+        $mail->SMTPSecure = strtoupper($_POST['email_encryption']) == "TLS" ? //Enable implicit TLS encryption
+            PHPMailer::ENCRYPTION_STARTTLS : PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = $_POST['email_port'];                                   //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
 
         //Recipients
-        $mail->setFrom('cosmoquestx@psi.edu', 'Mailer');
-        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-        $mail->addAddress('starstryder@gmail.com');               //Name is optional
+        $mail->setFrom($_POST['email_from'], 'CSB7.0 Server');
+        $mail->addAddress($_POST['rescue_email']);                            //Name is optional
         $mail->addReplyTo('info@example.com', 'Information');
         $mail->addCC('cc@example.com');
         $mail->addBCC('bcc@example.com');
 
         //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->isHTML(true);                                      //Set email format to HTML
         $mail->Subject = 'Here is the subject';
-        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->Body = 'This is the HTML message body <b>in bold!</b>';
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-        $mail->send();
-
-    echo 'Message has been sent';
+        if (!$mail->send()) {
+            echo json_encode(array('result' => false, 'message' => "Test Email failed: Something went wrong.", 'debug' => $mail->ErrorInfo));
+        } else {
+            echo json_encode(array('result' => true));
+        }
+//    echo 'Message has been sent';
     } catch (Exception $e) {
-//        echo json_encode(array('result' => false, 'debug' => $e, 'message' => "Test Email failed: " . $mail->ErrorInfo));
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        echo json_encode(array('result' => false, 'debug' => $e, 'message' => "Test Email failed: " . $mail->ErrorInfo));
+//    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 } else {
     echo json_encode(array('result' => false, 'message' => "Test Email failed: Empty request."));
