107cb2e48e4be5f5b9838b6163fe8f68bb5039b0
Pamela Gay <pamela@starstryder.com>
2024-08-11 20:31:39 -0500

    fixing FILE LOCATION

--- csb/science/tasks/add-images/add-images.php
+++ csb/science/tasks/add-images/add-images.php
@@ -189,11 +189,12 @@ function addImages()
 
     // Get the file location from the form
     $fileLocation = $_POST['fileLocation'];
-    $temp = $fileLocation."/".$line;
+
 
     // Loop through the rest of the file and add the images to the database
     while (!feof($file)) {
         $line = fgets($file);
+        $temp = $fileLocation.$line;
         if (!empty($line)) {
             if (isset($_POST['sun_angle']) && !empty($_POST['sun_angle'])) {
                 $query = "INSERT INTO images (image_set_id, application_id, name, file_location, sun_angle) VALUES (?, ?, ?, ?, ?)";
